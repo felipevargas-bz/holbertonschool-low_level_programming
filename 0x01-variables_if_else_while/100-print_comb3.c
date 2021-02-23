33828f3e2e347e36b08bea40f15d3ace15d01a52
felipevargas-bz <felipe15angel@gmail.com>
2021-02-23 11:51:51 -0500

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 90; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+
+		if (i < 90)
+		{
+
+			putchar(',');
+			putchar(' ');
+
+		}
+
+
+	}
+	putchar('\n');
+
+
+	return (0);
+
+
+}
