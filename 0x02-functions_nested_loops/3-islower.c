fd706012ed39cc1b6bdac7abb87c76fc724b2469
felipevargas-bz <felipe15angel@gmail.com>
2021-02-24 17:35:26 -0500

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _islower function that checks for lowercase character.
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+
+
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+
+		else
+			return (0);
+
+
+}
