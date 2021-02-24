0be7a9e838fef430159f9f9f7daad42180e06b02
felipevargas-bz <felipe15angel@gmail.com>
2021-02-24 16:34:49 -0500

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * main - function that prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 1; i <=10; i++)
+	{
+		for(letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+
+		}
+
+		_putchar('\n');
+	}
+
+
+}
