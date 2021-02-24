d1e7236e4ada4bba7caa6763dad63a682eded769
felipevargas-bz <felipe15angel@gmail.com>
2021-02-24 17:48:50 -0500

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * _islower function that checks for lowercase character.
- *
+ * _islower - function that checks for lowercase character.
+ * @c: letter to check
  * Return: Always 0 (Success)
  */
 int _islower(int c)
