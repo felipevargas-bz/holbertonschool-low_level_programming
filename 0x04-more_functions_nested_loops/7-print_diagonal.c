328856875545e2dd2764915f7aee104a6c7204a2
felipevargas-bz <felipe15angel@gmail.com>
2021-02-28 08:51:56 -0500

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints a square, followed by a new line.
+ * @n: number of times to print \
+ */
+void print_diagonal(int n)
+{
+	int i, c;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (c = 1; c < i; c++)
+		{
+			_putchar(' ');
+		}
+		if (n > 0)
+		{
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	if  (n < 1)
+	{
+		_putchar('\n');
+	}
+}
