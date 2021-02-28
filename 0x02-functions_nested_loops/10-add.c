9da2a684d8d934fdd07c6ad3dd88b5aafd9d0882
felipevargas-bz <felipe15angel@gmail.com>
2021-02-27 20:40:36 -0500

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result.
+ *
+ * @num1: number in question
+ * @num2: number in question
+ * Return: res
+ */
+int add(int num1, int num2)
+{
+	int res;
+
+	res = num1 + num2;
+
+	return (res);
+}
