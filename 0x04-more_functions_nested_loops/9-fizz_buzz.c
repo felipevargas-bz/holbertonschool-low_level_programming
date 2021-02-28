0f8127de1c8fcb4ad2af61bc0640c84d0deec909
felipevargas-bz <felipe15angel@gmail.com>
2021-02-28 13:08:04 -0500

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,26 @@ int main(void)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 		else if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 
 		if (i == 100)
 		{
 			continue;
 		}
+		putchar(' ');
 	}
 	putchar('\n');
 
