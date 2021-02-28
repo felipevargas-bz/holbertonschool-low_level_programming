234bcb343bc91c7d17c00ad0f30bfd04da472aed
felipevargas-bz <felipe15angel@gmail.com>
2021-02-27 22:51:18 -0500

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * mul - multiply two whole numbers
+ * @a: number in question
+ * @b: number in question
+ * Return: Always res (Success)
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+
+}
