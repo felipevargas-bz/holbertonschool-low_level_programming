8353d4c8068eb1d828ccefa1b1b4a82136a6fc5d
felipevargas-bz <felipe15angel@gmail.com>
2021-03-01 14:11:32 -0500

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * swap_int - Swap the values ​​of two pointers
+ * @a: pointer in question
+ * @b: pointer in question
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = a;
+	a = b
+	b = c
+}
