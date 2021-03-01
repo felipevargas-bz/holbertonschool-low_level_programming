71716df4f35c842b7566a347a8e1f9a28bc68250
felipevargas-bz <felipe15angel@gmail.com>
2021-02-28 22:08:20 -0500

    modify_header

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,6 +2,22 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 void positive_or_negative(int num);
 int largest_number(int a, int b, int c);
 
