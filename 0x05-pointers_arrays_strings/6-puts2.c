c1f60ef48b2f5c98a84b488c1a1f832982651226
felipevargas-bz <felipe15angel@gmail.com>
2021-03-02 22:00:42 -0500

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by
+ * a new line.
+ * @str: string in question
+ */
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str += 2;
+}
+	_putchar('\n');
+}
