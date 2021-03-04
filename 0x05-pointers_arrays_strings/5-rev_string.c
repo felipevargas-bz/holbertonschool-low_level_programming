ad7a596a851f739f66e93247b9b004558835aea7
felipevargas-bz <felipe15angel@gmail.com>
2021-03-04 14:37:11 -0500

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,29 +5,18 @@
  */
 void rev_string(char *s)
 {
-	int i = _strlen(s);
 	char tmp;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
 	for (int iz = 0, der = (i - 1); iz < (i / 2); iz++, der--)
 	{
 		tmp = s[iz];
 		s[iz] = s[der];
 		s[der] = tmp;
-
 	}
-}
-/**
- * _strlen - returns the length of a string.
- * @str: string in question.
- * Return: i.
- */
-int _strlen(char *str)
-{
-int i = 0;
-while (*s != '\0')
-{
-s++;
-i++;
-}
-return (i);
 }
