3b3c342ef29f24938ae781476f4b2c5c5d45ba97
felipevargas-bz <felipe15angel@gmail.com>
2021-03-04 17:33:00 -0500

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- * rev_string - reverses a string.
- * @s: string in question
+ * rev_string -  reverses a string.
+ * @s: string in question:
  */
 void rev_string(char *s)
 {
-	char tmp;
 	int i = 0, iz, der;
+	char tmp;
 
-	while (s[i] != '\0')
+	while (s[i]  != '\0')
 	{
 		i++;
 	}
 
-	for (iz = 0, der = (i - 1); iz < (i / 2); iz++, der--)
+	for (iz = 0, der = (i - 1); iz < (i / 2); iz ++, der--)
 	{
 		tmp = s[iz];
 		s[iz] = s[der];
