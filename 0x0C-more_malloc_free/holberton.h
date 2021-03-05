72c267821e0c5b0dafd96f3d150b83928f9eea9c
felipevargas-bz <felipe15angel@gmail.com>
2021-03-05 17:26:59 -0500

    modify_header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -29,5 +29,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
