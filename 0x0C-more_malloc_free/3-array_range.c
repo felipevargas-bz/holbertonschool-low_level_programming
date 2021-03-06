c44055bed3ae0a89f87587d68c046668df851198
felipevargas-bz <felipe15angel@gmail.com>
2021-03-06 13:53:42 -0500

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	gt = malloc(t * (sizeof(int)));
 	if (gt == NULL)
 		return (NULL);
-	for (i = min; min <= max; i++, min++)
+	for (i = min; i <= t; i++, min++)
 	{
 		gt[i] = min;
 	}
