722f346bb07434488b80e3d79616b2ac220b67e5
felipevargas-bz <felipe15angel@gmail.com>
2021-03-06 15:48:13 -0500

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: pointer.
+ */
+void puts_half(char *str)
+{
+	int t = 0, i;
+
+	while (str[t] != '\0')
+	{
+		t++;
+	}
+
+	for (i = (t / 2); i < t; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
