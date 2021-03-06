b4749e202f228f9a01f4963548a581f92ce2ba62
felipevargas-bz <felipe15angel@gmail.com>
2021-03-05 20:29:09 -0500

    modify_header

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -30,5 +30,6 @@ void puts2(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif
