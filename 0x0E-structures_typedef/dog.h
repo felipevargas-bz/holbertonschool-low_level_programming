50cb50a6f1a9f9cf220fbbe62dea0697c7aaaaa8
felipevargas-bz <felipe15angel@gmail.com>
2021-03-08 15:45:17 -0500

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct with dates.
+ * @name: name.
+ * @age: age.
+ * @owner: owner.
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+
+#endif
