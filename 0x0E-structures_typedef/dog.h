173c50714138c906677284ae00221cad4a7bd0ae
felipevargas-bz <felipe15angel@gmail.com>
2021-03-09 06:54:08 -0500

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,14 +6,14 @@
  * @age: age.
  * @owner: owner.
  */
-typedef struct dog
+typedef struct dog_t
 {
 char *name;
 float age;
 char *owner;
-} dog;
+} dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(struct dog_t *d, char *name, float age, char *owner);
+void print_dog(struct dog_t *d);
 
 #endif
