784753e34667886e69296ddf8d0a7131620e3432
felipevargas-bz <felipe15angel@gmail.com>
2021-03-13 15:56:50 -0500

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,35 +10,31 @@ char *str_concat(char *s1, char *s2)
 	int t = 0, size = 0, i;
 	char *ptr = NULL;
 
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+
 	if (s1 != NULL)
 	{
 		while (s1[t] != '\0')
-		{
 			t++;
-		}
 	}
-
 	size = size + t;
 	t = 0;
 	if (s2 != NULL)
 	{
 		while (s2[t] != '\0')
-		{
 			t++;
-		}
 	}
 
 	size = size + t;
 	ptr = malloc(sizeof(char) * size + 1);
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		ptr[i] = s1[i];
-	}
+
 	t = 0;
 	for (; s2[t] != '\0'; i++)
 	{
