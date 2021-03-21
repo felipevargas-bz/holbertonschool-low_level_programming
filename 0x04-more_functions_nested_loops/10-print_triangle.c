05d6713371cbf823c94ec806de775d6e5449d2fc
felipevargas-bz <felipe15angel@gmail.com>
2021-03-20 20:57:19 -0500

    task10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,17 +8,17 @@ void print_triangle(int size)
 	int a, b;
 
 	if (size <= 0)
-		putchar('\n');
+		_putchar('\n');
 	for (a = 1; a <= size; a++)
 	{
 		for (b = 1; b <= size; b++)
 		{
 			if (b <= (size - a))
-				putchar(' ');
+				_putchar(' ');
 			else
-				putchar('#');
+				_putchar('#');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 
 }
