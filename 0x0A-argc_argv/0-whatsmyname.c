61aa4042e388431840c569e6d93978628958f117
felipevargas-bz <felipe15angel@gmail.com>
2021-03-21 20:10:03 -0500

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - print name.
+ * @argc: number of parameters.
+ * @argv: array of parameters.
+ * Return: 0 always.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc)
+	{
+	}
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
