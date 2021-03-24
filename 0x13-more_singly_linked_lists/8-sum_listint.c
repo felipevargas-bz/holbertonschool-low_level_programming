b10977e868a7a3d06e596a6438161dc67edcc05f
felipevargas-bz <felipe15angel@gmail.com>
2021-03-24 10:38:22 -0500

    task8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data
+ * (n) of a listint_t linked list.
+ * @head: pointer to node.
+ * Return: sum of dates of nodes.
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *aux = head;
+	int sum = aux->n;
+
+	if (!head)
+	{
+		return (0);
+	}
+	while (aux->next != NULL)
+	{
+		aux = aux->next;
+		sum += aux->n;
+	}
+	return (sum);
+}
