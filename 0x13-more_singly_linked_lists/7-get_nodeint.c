5d04ab1e817be732079bb1d496282fac3fab5bdc
felipevargas-bz <felipe15angel@gmail.com>
2021-03-23 22:44:56 -0500

    task7 co

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *aux;
 	unsigned int iterator = 0;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
+
 	if (head)
 	{
 		aux = head;
@@ -23,5 +28,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			}
 		}
 	}
-	return (NULL);
+		return (0);
 }
