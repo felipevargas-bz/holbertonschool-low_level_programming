e1fccfe182e4a0f550a289f128c804ff998ec8d3
felipevargas-bz <felipe15angel@gmail.com>
2021-03-24 19:28:11 -0500

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = node;
 		return (*head);
 	}
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+	}
 	while (aux)
 	{
 		if ((idx - 1) == iterator)
