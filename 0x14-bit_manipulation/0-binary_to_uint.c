219699928f1c1dda0bc999f01217fbb665c73e0a
felipevargas-bz <felipe15angel@gmail.com>
2021-03-25 18:18:05 -0500

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,11 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int pot = 1;
 	unsigned int bit = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
+
 	while (*b != '\0')
 	{
 		if (*b == '1' || *b == '0')
