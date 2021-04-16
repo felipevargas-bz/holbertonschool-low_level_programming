cffc212e7a8e72c7a6c1e80289111bc642836427
felipevargas-bz <felipe15angel@gmail.com>
2021-04-16 10:43:06 -0500

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,16 @@
  */
 char *leet(char *str)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[i])
 	{
-		while (j <= 7)
+		for (j = 0; j <= 7; j++)
 		{
 			if (str[i] == leet[j] ||
 			    str[i] - 32 == leet[j])
 				str[i] = j + '0';
-			j++;
 		}
 
 		i++;
