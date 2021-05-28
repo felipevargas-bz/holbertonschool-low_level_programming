6ac71380036236b0edd13991022f6612bd1d35ac
felipevargas-bz <felipe15angel@gmail.com>
2021-05-27 21:06:52 -0500

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash_table.
+ * @size: size of hast table.
+ * Return: hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+	unsigned long int index = 0;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+	while (index <= size)
+	{
+		table->array[index] = NULL;
+		index++;
+	}
+	return (table);
+}
+
