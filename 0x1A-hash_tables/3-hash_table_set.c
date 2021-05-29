b508c0bdf8b36fea7b658313f165abad54d0de76
felipevargas-bz <felipe15angel@gmail.com>
2021-05-28 19:51:01 -0500

    TASK3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+hash_node_t *add(const char *key, const char *value);
 /**
  * hash_table_set - adds an element to the hash table.
  * @ht: hash table.
@@ -8,34 +9,63 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new = NULL;
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *aux = ht->array[index];
+	hash_node_t *node, *aux;
+	unsigned long int index = key_index(key, ht->size);
+	char *new_value;
 
-	if (ht == NULL || key == NULL || *key == '\0' || index >= ht->size)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
+	aux = ht->array[index];
+	while (aux != NULL)
+	{
+		if (strcmp(aux->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(aux->value);
+			return (1);
+		}
+		aux = aux->next;
+	}
+	node = add(key, value);
+	if (node == NULL)
 		return (0);
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
-	if (ht->array[index] == NULL)
-	{
-		new->value = (char *)value;
-		new->key = (char *)key;
-		ht->array[index] = new;
-
-		return (1);
-	}
-	else
-	{
-		new->value = (char *)value;
-		new->key = (char *)key;
-		ht->array[index] = new;
-		new->next = aux;
-
-		return (1);
-	}
-	return (0);
+	return (1);
 }
+/**
+ * add - add a node.
+ * @key: key.
+ * @value: value.
+ * Return: a node of the type hash_node_t.
+ */
+hash_node_t *add(const char *key, const char *value)
+{
+	hash_node_t *node;
 
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+
+		return (NULL);
+	}
+	node->next = NULL;
+
+	return (node);
+}
